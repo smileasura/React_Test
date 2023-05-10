33737bc3f7fd71f31fe14dd8b4e2d0ce50cb3aaf
smileasura <smileless@naver.com>
2023-05-10 09:41:02 +0900

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
+import { useState } from "react";
 import { ColoredMessage } from "./components/ColoredMessage";
 
 export const App = () => {
-    const onClickButton = () => {
-      alert();  
-    };
+  const [num, setNum] = useState(0);
 
-    return (
-    <>
-        <h1 style={{color:"red"}}>안녕하세요!</h1>
-        {/* <ColoredMessage color="blue" message="잘 지내시죠?"/> */}
-        <ColoredMessage color="blue">잘 지내시죠?</ColoredMessage>
-        <ColoredMessage color="pink">잘 지냅니다!</ColoredMessage>
-        <button onClick={onClickButton}>버튼</button>
-    </>
-    );
+  const onClickButton = () => {
+    //alert();
+    setNum(num + 1);  
+  };
+
+  return (
+  <>
+      <h1 style={{color:"red"}}>안녕하세요!</h1>
+      {/* <ColoredMessage color="blue" message="잘 지내시죠?"/> */}
+      <ColoredMessage color="blue">잘 지내시죠?</ColoredMessage>
+      <ColoredMessage color="pink">잘 지냅니다!</ColoredMessage>
+      <button onClick={onClickButton}>버튼</button>
+      <p>{num}</p>
+  </>
+  );
 };
