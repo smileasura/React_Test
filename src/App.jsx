56479d23cd7b33c0e46bf4429501e48589705142
smileasura <smileless@naver.com>
2023-05-10 09:44:46 +0900

    1

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { ColoredMessage } from "./components/ColoredMessage";
 
 export const App = () => {
+  console.log("Rendering");
   const [num, setNum] = useState(0);
 
   const onClickButton = () => {
